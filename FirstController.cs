e138f0e58fd08f2dad96aaca2d5e7b7562428637
Lucas Eduardo <lucasatletico@hotmail.com>
2021-09-18 18:53:12 -0300

    first commit

---
+++ FirstController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace DayOne
+{
+    [Route("test")]
+    public class FirstController : ControllerBase //(crtl + .)
+    {
+        [HttpGet("getvalue")]
+        public string RetornaValor(
+            [FromQuery] string name,
+            [FromQuery] DateTime? nascimento
+            )
+        {
+            if (nascimento == null)
+            {
+                throw new Exception("data invalida");
+            }
+
+            var idade = (int)(DateTime.Now - nascimento.Value).TotalDays / 365;
+
+            return "Seu nome é " + name + ", e sua idade é: " + idade.ToString();
+        }
+       
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DayOnept02
+{
+    public class Person
+    {
+        public PersonName Name { get; set; } = new PersonName();
+        public PersonName Name { get; set; }
+    }
+}
